72a2a9d3198833ad253831f9f4b8982d0ba6a54f
Preston Abraham <112017483+preston-abraham@users.noreply.github.com>
2023-01-20 13:37:47 -0600

    Update r_to_py.py

--- r_to_py.py
+++ r_to_py.py
@@ -7,7 +7,16 @@ openai.organization = "org-eptWwJzwl8LLZVNyAH1xBxbF"
 openai.api_key = st.secrets['api_key']
 
 
-st.title('R to Python Re-Writer')
+
+st.title('R <---> Python Re-Writer')
+
+
+mode = st.selectbox('Mode',['R to Python','Python to R'])
+
+
+prepend = 'R code in Python'
+if mode == 'Python to R':
+    prepend = 'Python code in R'
 
 code = st.text_area('Code to rewrite')
 response = ''
@@ -15,7 +24,7 @@ response = ''
 if st.button('Re-write'):
     response = passions_response = openai.Completion.create(
                           model="text-davinci-003",
-                          prompt = 'Rewrite the following R code in Python with good comments:\n'+code+'\n',
+                          prompt = 'Rewrite the following ' + prepend + ' with good comments:\n'+code+'\n',
                           temperature = 0.15,
                           top_p = 1,
                           max_tokens = 500
