7feaaa9fa12316087be2bc64a6920e0ced8dd3b8
Preston Abraham <112017483+preston-abraham@users.noreply.github.com>
2023-01-25 13:49:55 -0600

    Update r_to_py.py

--- r_to_py.py
+++ r_to_py.py
@@ -11,25 +11,39 @@ openai.api_key = st.secrets['api_key']
 st.title('R <---> Python Re-Writer')
 
 
-mode = st.selectbox('Mode',['R to Python','Python to R'])
+mode = st.selectbox('Mode',['R to Python','Python to R','Natural language to Python'])
 
+if not mode == 'Natural language to Python':
+    prepend = 'R code in Python'
+    if mode == 'Python to R':
+        prepend = 'Python code in R'
 
-prepend = 'R code in Python'
-if mode == 'Python to R':
-    prepend = 'Python code in R'
+    code = st.text_area('Code to rewrite')
+    response = ''
 
-code = st.text_area('Code to rewrite')
-response = ''
+    if st.button('Re-write'):
+        response = openai.Completion.create(
+                              model="text-davinci-003",
+                              prompt = 'Rewrite the following ' + prepend + ' with good comments:\n'+code+'\n',
+                              temperature = 0.15,
+                              top_p = 1,
+                              max_tokens = 500
+                            )["choices"][0]["text"]
+        warnings.warn(str(code) + '\n' + str(response))
+    st.text(response)
+else:
+    code = st.text_area('Code to Write')
+    response = ''
 
-if st.button('Re-write'):
-    response = passions_response = openai.Completion.create(
-                          model="text-davinci-003",
-                          prompt = 'Rewrite the following ' + prepend + ' with good comments:\n'+code+'\n',
-                          temperature = 0.15,
-                          top_p = 1,
-                          max_tokens = 500
-                        )["choices"][0]["text"]
-    warnings.warn(str(code) + '\n' + str(response))
-st.text(response)
+    if st.button('Write'):
+        response = openai.Completion.create(
+                              model="text-davinci-003",
+                              prompt = 'Write Python code to do the following: '+code+'\n',
+                              temperature = 0.15,
+                              top_p = 1,
+                              max_tokens = 500
+                            )["choices"][0]["text"]
+        warnings.warn(str(code) + '\n' + str(response))
+    st.text(response)
 
 
