4c25f70326dd56971be0348e9bf34ffcf3dcd15c
Preston Abraham <112017483+preston-abraham@users.noreply.github.com>
2023-02-23 16:45:21 -0600

    Add SQL writing

--- r_to_py.py
+++ r_to_py.py
@@ -11,9 +11,9 @@ openai.api_key = st.secrets['api_key']
 st.title('R <---> Python Re-Writer')
 
 
-mode = st.selectbox('Mode',['R to Python','Python to R','Natural language to Python'])
+mode = st.selectbox('Mode',['R to Python','Python to R','Natural language to Python','Natural language to SQL'])
 
-if not mode == 'Natural language to Python':
+if not 'Natural language' in mode:
     prepend = 'R code in Python'
     if mode == 'Python to R':
         prepend = 'Python code in R'
@@ -34,11 +34,11 @@ if not mode == 'Natural language to Python':
 else:
     code = st.text_area('Code to Write')
     response = ''
-
+    lan = mode.split(' ')[3]
     if st.button('Write'):
         response = openai.Completion.create(
                               model="text-davinci-003",
-                              prompt = 'Write Python code to do the following: '+code+'\n',
+                              prompt = 'Write '+lan+' code to do the following: '+code+'\n',
                               temperature = 0.15,
                               top_p = 1,
                               max_tokens = 500
